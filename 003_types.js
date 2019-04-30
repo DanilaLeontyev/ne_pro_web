2c5da107c64139f1a306278f558f979efde9f006
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2019-04-30 14:06:50 +0500

    003 done

---
+++ 003_types.js
@@ -0,0 +1,15 @@
+let typeNumber = 2.5 // number
+let typeString = 'someString' // string
+let typeBoolean = true // boolean
+let typeNull = null // special object
+let typeUndefined = undefined // undefined
+let typeObject = { name: 'Jon' } // object
+
+typeof (typeNumber) // get type of variable
+
+console.log(typeof (typeNumber));
+console.log(typeof (typeString));
+console.log(typeof (typeBoolean));
+console.log(typeof (typeNull));
+console.log(typeof (typeUndefined));
+console.log(typeof (typeObject));
