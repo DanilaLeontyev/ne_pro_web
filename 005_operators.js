de1c49e070da5e7e9db8c732c4ee89a82d6a6ab3
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2019-04-30 18:47:06 +0500

    005 done

---
+++ 005_operators.js
@@ -0,0 +1,14 @@
+let unarPlus = 2 + 4; // 6
+let unarNeg = 4 - 3; // 1
+let concat = 'some ' + 'value'; // some value
+let multiplication = 5 * 5; // 25
+let division = 10 / 5; // 2
+let remainder = 5 % 2; // 1 
+let exponential = 2 ** 3 // 8  // 2*2*2
+
+let counter = 0;
+console.log(++counter) // 1
+
+let counter = 0;
+console.log(counter++) // still 0
+console.log(counter); // now 1
