dade88acb41e6d3f16171dcdeeef1c6de3e9e05c
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2019-04-30 21:47:10 +0500

    006 done

---
+++ 006_comparisons.js
@@ -0,0 +1,26 @@
+// ASSIGMENT
+let val = 0;
+val += 3; // val = val + 3
+val -= 3; // val = val - 3
+val *= 3; // val = val * 3
+val /= 3; // val = val / 3
+
+// COMPARISON
+let x = 5;
+x === 5 // true
+x === '5' // false
+x !== 5 // false
+x !== 8 // true
+x > 5 // false
+x < 8 // true
+x >= 5 // true
+x >= 8 // false 
+x <= 5 // true
+
+// LOGICAL
+// AND
+(x < 10 && y > 1) // true
+// OR
+(x === 5 || y === 5) // false
+// NOT
+!(x == y) // true
