2e50884769426fb6c8f29beb322442d8b5ed9b18
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2019-04-30 13:05:03 +0500

    002 complete

---
+++ 002_variable.js
@@ -0,0 +1,9 @@
+var x = 'x'; // Outdate
+let y = 'y';
+const z = 'z';
+
+// let can change
+y = 'newY';
+
+// Error: const can`t change
+//z = 'newZ'; 
--- src/002_variable.js
+++
@@ -1,7 +0,0 @@
-const z = 'Z';
-let x = 'X';
-var y = 'Y'; // не используется
-
-console.log(x);
-console.log(y);
-console.log(z);
