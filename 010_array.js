ef4deddb863869bbad240da7cbf705f277d8f9a7
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2019-05-05 12:52:38 +0500

    010 done

---
+++ 010_array.js
@@ -0,0 +1,18 @@
+let donutBox = [
+    'Cinnamon',
+    'Jelly',
+    'Chocolate',
+    'Banana',
+    'Apple',
+    'Strawberry'
+];
+
+console.log(donutBox);
+
+// replace Cinnamon to Blueberry
+
+donutBox[0] = 'Blueberry';
+console.log(donutBox);
+
+// find the length of array
+console.log(donutBox.length);
